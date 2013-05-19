a9a19852d0583582d574042edc06ae2e7fb75a86
Alexander <oburakevych@gmail.com>
2013-05-19 17:09:12 +1000

    fixed the upload link

--- app/js/app.js
+++ app/js/app.js
@@ -8,16 +8,17 @@ var intimateModule = angular.module('intimateApp',
 		 'imUserRoleAssignmentModule']
 );
 /* LOCALHOST */
+/*
 intimateModule.constant("BASE_URL_BACKEND","http://localhost\\:8080//intimate-backend"); //For $resource
 intimateModule.constant("BASE_URL_PARSE_BACKEND","https://api.parse.com/1"); //For $resource
 intimateModule.BASE_URL_BACKEND = 'http://localhost\:8080/intimate-backend'; // For $http
-
+*/
 /* secretroom.intimate-app.com */
-/*
+
 intimateModule.constant("BASE_URL_BACKEND","http://secretroom.intimate-app.com\\:8080//intimate-backend"); //For $resource
 intimateModule.constant("BASE_URL_PARSE_BACKEND","https://api.parse.com/1"); //For $resource
 intimateModule.BASE_URL_BACKEND = 'http://secretroom.intimate-app.com\:8080/intimate-backend'; // For $http
-*/
+
 intimateModule.BASE_URL_DATA = 'data';
 
 intimateModule.config(['$routeProvider', function($routeProvider) {
