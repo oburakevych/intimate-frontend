e5e446ec43a9cbd6676fa11203eb4039e024ea9f
Alexander <oburakevych@gmail.com>
2013-05-21 14:06:56 +1000

    fixed the upload link

--- app/js/app.js
+++ app/js/app.js
@@ -8,17 +8,17 @@ var intimateModule = angular.module('intimateApp',
 		 'imUserRoleAssignmentModule']
 );
 /* LOCALHOST */
-/*
+
 intimateModule.constant("BASE_URL_BACKEND","http://localhost\\:8080//intimate-backend"); //For $resource
 intimateModule.constant("BASE_URL_PARSE_BACKEND","https://api.parse.com/1"); //For $resource
 intimateModule.BASE_URL_BACKEND = 'http://localhost\:8080/intimate-backend'; // For $http
-*/
-/* secretroom.intimate-app.com */
 
+/* secretroom.intimate-app.com */
+/*
 intimateModule.constant("BASE_URL_BACKEND","http://secretroom.intimate-app.com\\:8080//intimate-backend"); //For $resource
 intimateModule.constant("BASE_URL_PARSE_BACKEND","https://api.parse.com/1"); //For $resource
 intimateModule.BASE_URL_BACKEND = 'http://secretroom.intimate-app.com\:8080/intimate-backend'; // For $http
-
+*/
 intimateModule.BASE_URL_DATA = 'data';
 
 intimateModule.config(['$routeProvider', function($routeProvider) {
@@ -26,6 +26,6 @@ intimateModule.config(['$routeProvider', function($routeProvider) {
           when('/front-page', {templateUrl: 'views/front-page.html'}).
 		  when('/upload', {templateUrl: 'views/upload.html'}).
 		  when('/safe-room', {templateUrl: 'views/safe-room.html'}).
-		  when('/player', {templateUrl: 'players/VideoOnDemandStreaming/FlashRTMPPlayer/player.html'}).
+		  when('/player', {templateUrl: 'views/player.html'}).
 	      otherwise({redirectTo: '/safe-room'});
 	}]);
