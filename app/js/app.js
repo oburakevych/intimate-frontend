19c6d6abe33963b5218863efd3ca69265e6e107a
oburakevych <oburakevych@gmail.com>
2013-05-30 15:31:22 +1000

    Clean-up for the commercial project

--- app/js/app.js
+++ app/js/app.js
@@ -1,32 +1,24 @@
 'use strict';
 
-/* EdgeBud App Module */
+/* IntiMate App Module */
 
-var intimateModule = angular.module('intimateApp',
-		[ 'filterModule',
-		 'imUserModule',
-		 'imUserRoleAssignmentModule']
-);
+var intimateModule = angular.module('intimateApp', []);
 /* LOCALHOST */
-
+/*
 intimateModule.constant("BASE_URL_BACKEND","http://localhost\\:8080//intimate-backend"); //For $resource
 intimateModule.constant("BASE_URL_PARSE_BACKEND","https://api.parse.com/1"); //For $resource
 intimateModule.BASE_URL_BACKEND = 'http://localhost\:8080/intimate-backend'; // For $http
-
+*/
 /* secretroom.intimate-app.com */
-/*
 intimateModule.constant("BASE_URL_BACKEND","http://secretroom.intimate-app.com\\:8080//intimate-backend"); //For $resource
 intimateModule.constant("BASE_URL_PARSE_BACKEND","https://api.parse.com/1"); //For $resource
 intimateModule.BASE_URL_BACKEND = 'http://secretroom.intimate-app.com\:8080/intimate-backend'; // For $http
-*/
+
 intimateModule.BASE_URL_DATA = 'data';
 
 intimateModule.config(['$routeProvider', function($routeProvider) {
 	  $routeProvider.
-          when('/front-page', {templateUrl: 'views/front-page.html'}).
-		  when('/upload', {templateUrl: 'views/upload.html'}).
           when('/safe-room/:roleName', {templateUrl: 'views/safe-room.html'}).
 		  when('/safe-room', {templateUrl: 'views/safe-room.html'}).
-		  when('/player', {templateUrl: 'views/player.html'}).
 	      otherwise({redirectTo: '/safe-room'});
 	}]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,52 +1,5 @@
 'use strict';
 
-function SignupController($scope, $rootScope, $timeout, SecurityService) {
-	$scope.owner;
-	$scope.signup = function(successFn, errorFn) {
-		console.log("Signup " + angular.toJson($scope.owner));
-		SecurityService.signup($scope.owner, $scope, successFn, errorFn);
-	}
-}
-
-function UserSignupLoginController($scope, $rootScope, $cookieStore, $timeout, SecurityService) {
-	$scope.haveAccount = false;
-	$scope.owner;
-
-	$scope.signup = function(successFn, errorFn) {
-		console.log("Signup " + angular.toJson($scope.owner));
-		SecurityService.signup($scope.owner, $scope, successFn, errorFn);
-	}
-
-	$scope.login = function(successFn, errorFn) {
-		console.log("Login " + angular.toJson($scope.owner));
-		SecurityService.login($scope.owner, $scope, successFn, errorFn);
-	}
-
-	$scope.logout = function() {
-		console.log("Logging out " + angular.toJson($rootScope.authorisedOwner));
-		SecurityService.logout();
-	}
-
-	$scope.afterUserLoggedIn = function() {
-		console.log("LoginSignupUserController: After user logged In");
-	}
-
-	$scope.afterUserLoggedOut = function() {
-		console.log("LoginSignupUserController: After user logged Out");
-	}
-
-	$scope.$on('event:after-user-logged-in', $scope.afterUserLoggedIn);
-	$scope.$on('event:after-user-logged-out', $scope.afterUserLoggedOut);
-}
-
-function UploadController($scope, $rootScope, $timeout) {
-	$timeout(function() {
-		if ($rootScope.authorisedOwner && r && r.opts) {
-			r.opts.query.ownerId = $rootScope.authorisedOwner.id;
-		}
-	}, 1000);
-}
-
 function UserRoleAssignmentController($scope, $rootScope, $timeout, UserRoleAssignmentService) {
 	$scope.user = {};
 	$scope.roles = {
