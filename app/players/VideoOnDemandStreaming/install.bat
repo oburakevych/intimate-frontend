fbc6523780b44d668ea3c136c511416e7d780e0e
oburakevych <oburakevych@gmail.com>
2013-05-30 16:06:16 +1000

    removed unused files

--- app/players/VideoOnDemandStreaming/install.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-echo Installing VideoOnDemandStreaming...
-xcopy "%WMSAPP_HOME%\examples\VideoOnDemandStreaming\conf" "%WMSAPP_HOME%\conf" /s  /Y  /Q > NUL
-IF NOT EXIST "%WMSAPP_HOME%\applications\vod" mkdir "%WMSAPP_HOME%\applications\vod"
-IF NOT "%1" == "all" pause
